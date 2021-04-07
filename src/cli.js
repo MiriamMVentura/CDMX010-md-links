9784c58f99bf3ac27c3374c71ef8af176dc03697
MiriamMVentura <miriamartv@gmail.com>
2021-04-07 03:56:15 -0500

    feat(findFiles): filter all md files by extension

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,49 @@
+#!/usr/bin/env node
 'use strict';
+const chalk = require('chalk');
+const art = require('ascii-art');
+// const inquirer = require('inquirer');
+// const process = require('process');
+const modulo = require('./index');
+
+
+
+
+// --- modulo de  saludo e instrucciones sueltas que llamaré poco a poco
+modulo.intro();
+console.log(modulo.description);
+
+
+// --- cabecera con nombre de la cli
+function message (mess) {
+  art.font(mess, 'doom', (err, rederer) => {
+    console.log(chalk.bold.magentaBright(err || rederer))
+  })
+}
+
+// function selectFile () {
+//   const option = [{
+//       name: 'type',
+//       type: 'list',
+//       message: 'Selecciona el archivo a leer: ',
+//       choices: ['main()']
+//     },
+//   ];
+//   return inquirer.prompt(option);
+// };
+// selectFile();
+
+
+
+// message('Glitch-CLI');
+// message('hola');
+
+
+// IIFE (Immediately Invoked Function Expression)
+
+// (async() => {
+//   message('Glitch-CLI');
+//  await intro();
+// })();
+
+
--- src/index.js
+++ src/index.js
@@ -1,57 +1,78 @@
 #!/usr/bin/env node
 'use strict';
 const path = require('path');
-const fs = require('fs');
-const modulo = require('./mdLinks');
+const fs = require('fs').promises;
+const fsr = require('fs');
+const fetch = require('node-fetch');
+const chalk = require('chalk');
 
-// -- quiero comproabar si la ruta de los archivos y directorios son validos
+// creando el módulo de vienvenida
+function intro() {
+  console.log(chalk.bold.blue('Hola, bienvenidx a Glitch-CLI'));
+  console.log(chalk.bold.blue('archivos filtrados...'));
+}
 
-// -- quiero ver todos los arcivos que contiene mi carpeta "directory"
-// fs.readdir('./src/directory', (error, files) => {
-//   if (error) {
-//     throw error
-//   }
-//   console.log('archivos encontrados:');
-//   console.log(files);
-// });
 
-// --- quiero saber las rutas de mis archivos
+// -- quiero ver todos los arcivos .md que contiene mi carpeta "directory" sin importar el nivel
+async function main() {
+  console.log(await findFiles(path.join(__dirname, 'directory')));
+}
+main();
 
+async function findFiles(folderName) {
+  let allFiles = [];
+  const items = await fs.readdir(folderName, { withFileTypes: true });
+  for (const item of items) {
+    if (item.isDirectory()) {
+      allFiles = allFiles.concat(
+        await findFiles(path.join(folderName, item.name))
+      );
+    } else {
+      if (path.extname(item.name) === '.md') {
+        allFiles.push(path.join(folderName, item.name));
+      }
+    }
+  }
+  return allFiles;
+}
 
 
 // // ---- quiero leer el contenido de un archivo  e imprimirlo en la consola
 // function read(ruta, cb) {
-//   fs.readFile(ruta, 'UTF-8', (err, data) => {
-//     cb(data);
-//   })
-// }
-// read(__dirname + '/directory/definitionGlitch.txt', console.log)
+//   fsr.readFile(ruta, 'UTF-8', (err, data) => {
+//       cb(data)
+//     })
+//   }
+//   read(__dirname + '/directory/glitchArtGenerator.md', console.log)
+
 
 
 
 
 // // --- quiero saber la extención de mi archivo
 
-// --- modulo de  saludo e instrucciones sueltas que llamaré poco a poco
-modulo.intro();
-console.log(modulo.stepOne);
 
-fs.readdir('./src/directory', (error, files) => {
-    if (error) {
-        console.error('No he podido leer el directorio', error);
-    } else {
-        console.log(files);
-    }
-    fs.readFile('./src/directory/glitchArtGenerator.md', 'UTF-8', (error, data) => {
-      if (error) {
-        console.error('No he podido leer el archivo', error);
-    } else {
-        console.log(data);
-    }
-    });
-    console.log('El archivo "glitchArtGenerator.md" contiene lo siguiente:');
-  })
 
-// module.exports = () => {
-//   // ...
-// };
+// fs.readdir('./src/directory', (error, files) => {
+//     if (error) {
+//         console.error('No he podido leer el directorio', error);
+//     } else {
+//         console.log(files);
+//     }
+//     fs.readFile('./src/directory/glitchArtGenerator.md', 'UTF-8', (error, data) => {
+//       if (error) {
+//         console.error('No he podido leer el archivo', error);
+//     } else {
+//         console.log(data);
+//     }
+//     });
+//     console.log('El archivo "glitchArtGenerator.md" contiene lo siguiente:');
+// })
+
+
+module.exports = {
+  intro,
+  description: 'Estos son todos los archivos .md encontrados:',
+  // main,
+  // findFiles,
+};
